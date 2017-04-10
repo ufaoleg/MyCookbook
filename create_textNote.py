e6c96fbd1a1ae7ce0f28f3efa88238e86e3c61fd
ufaoleg <ufaoleg@bk.ru>
2017-04-10 22:04:07 +0500

    commit

---
+++ create_textNote.py
@@ -0,0 +1,81 @@
+# coding=utf-8
+# Copyright(c) 2017, Oleg Rezvov
+# @PRSPKT, http://prspkt.ru
+__author__ = "ООО АБ Проспект"
+
+# region import
+import clr
+
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import Element wrapper extension methods
+clr.AddReference("RevitNodes")
+import Revit
+
+clr.ImportExtensions(Revit.Elements)
+
+# Import geometry conversion extension methods
+clr.ImportExtensions(Revit.GeometryConversion)
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+view = UnwrapElement(IN[0])
+txt = [n for n in IN[1]]
+baseVec = XYZ(1, 0, 0)
+upVec = XYZ(0, 1, 0)
+convert = 0.00328084
+
+point = [XYZ(i.X*convert, i.Y*convert, i.Z) for i in IN[2]]
+
+try:
+    errorReport = None
+    TransactionManager.Instance.EnsureInTransaction(doc)
+
+    collector = FilteredElementCollector(doc, view.Id)
+    text_elements = collector.OfCategory(BuiltInCategory.OST_TextNotes)
+
+    for element in text_elements:
+        element_id = UnwrapElement(element).Id
+        doc.Delete(element_id)
+
+    for i, k in enumerate(point):
+        len = txt[i].Length * 0.0066
+        note = doc.Create.NewTextNote(view, k, baseVec, upVec, len, TextAlignFlags.TEF_ALIGN_CENTER, txt[i])
+
+    TransactionManager.Instance.TransactionTaskDone()
+
+#OUT = note
+except:  # if error accurs anywhere in the process catch it
+    import traceback
+
+    errorReport = traceback.format_exc()  # End Transaction
+    TransactionManager.Instance.TransactionTaskDone()
+
+# Assign your output to the OUT variable
+if errorReport == None:
+    OUT = "OK"
+else:
+    OUT = errorReport
---
+++ rename_doors.py
@@ -0,0 +1,115 @@
+# coding=utf-8
+# Copyright(c) 2017, Oleg Rezvov
+# @PRSPKT, http://prspkt.ru
+__author__ = "ООО АБ Проспект"
+
+# region import
+import clr
+
+clr.AddReference('ProtoGeometry')
+from Autodesk.DesignScript.Geometry import *
+
+# Import Element wrapper extension methods
+clr.AddReference("RevitNodes")
+import Revit
+
+clr.ImportExtensions(Revit.Elements)
+
+# Import geometry conversion extension methods
+clr.ImportExtensions(Revit.GeometryConversion)
+
+# Import DocumentManager and TransactionManager
+clr.AddReference("RevitServices")
+import RevitServices
+from RevitServices.Persistence import DocumentManager
+from RevitServices.Transactions import TransactionManager
+
+doc = DocumentManager.Instance.CurrentDBDocument
+uiapp = DocumentManager.Instance.CurrentUIApplication
+app = uiapp.Application
+
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+import Autodesk
+from Autodesk.Revit.DB import *
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+
+def ProcessList(_func, _list):
+    return map(lambda x: ProcessList(_func, x) if type(x) == list else _func(x), _list)
+
+
+koef = 304.8
+
+if isinstance(IN[0], list):
+    elements = []
+    for i in IN[0]:
+        elements.append(UnwrapElement(i))
+else:
+    elements = [UnwrapElement(IN[0])]
+
+output = []
+output_elements = []
+
+try:
+    errorReport = None
+    for element in elements:
+        el_width = element.Symbol.LookupParameter('Ширина').AsDouble() * koef
+        el_height = element.Symbol.LookupParameter('Высота').AsDouble() * koef
+        el_mark = element.Symbol.LookupParameter('Маркировка типоразмера').AsString()
+        el_left_right = "Л" if element.Symbol.LookupParameter('Левая').AsValueString() == "Да" else ""
+        el_type_name = element.Name
+        el_EI = el_type_name[el_type_name.find("EI"):el_type_name.find("0") + 1]
+        if el_type_name.startswith("Д.Мет") or el_type_name.startswith("ДП.Мет"):
+            if "Люк" in el_type_name:
+                output.append("Люк металлический {} ({:.0f}-{:.0f}(h)) ГОСТ 31173-2003"\
+                              .format(el_left_right, el_height, el_width))
+                output_elements.append(element)
+            elif "Дус" in el_type_name:
+                output.append("ДУС {} {:.0f}-{:.0f} Серия 5.904-4"\
+                              .format(el_left_right, el_height, el_width))
+                output_elements.append(element)
+            elif "EI" not in el_type_name:
+                output.append("ДСВ {} ({:.0f}-{:.0f}(h)) ГОСТ 31173-2003"\
+                              .format(el_left_right, el_height, el_width))
+                output_elements.append(element)
+            else:
+                output.append("ДПМ {} {} ({:.0f}-{:.0f}(h)) ГОСТ 53307-2009"\
+                              .format(el_EI, el_left_right, el_height, el_width))
+                output_elements.append(element)
+        if el_type_name.startswith("Д.Дер") or el_type_name.startswith("ДП.Дер"):
+            if "EI" not in el_type_name:
+                output.append("ДГ {}-{}{} ГОСТ 6629-88".format("21" if el_height == 2070 else str(el_height / 100)[:2],
+                                                               str(el_width / 100)[:1] if el_width < 1000 else str(
+                                                                   el_width / 100)[:2], el_left_right))
+                output_elements.append(element)
+            else:
+                # output.append("ДПМ " + el_EI + " (" + str(el_width) + "x" + str(el_height) + "(h)")
+                output.append("ДПД {} {} ({:.0f}-{:.0f}(h))".format(el_EI, el_left_right, el_height, el_width))
+                output_elements.append(element)
+        if "оджи" in el_type_name:
+            output.append("ДАН О Оп {} БПР Р {:.0f}х{:.0f}, ГОСТ 23747-2015" \
+                .format(el_left_right, el_height, el_width))
+            output_elements.append(element)
+
+
+
+except:  # if error accurs anywhere in the process catch it
+    import traceback
+
+    errorReport = traceback.format_exc()  # End Transaction
+    TransactionManager.Instance.TransactionTaskDone()
+
+# Assign your output to the OUT variable
+if errorReport == None:
+    OUT = [output_elements, output]
+else:
+    OUT = errorReport
