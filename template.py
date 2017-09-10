e42444eb21b5630a0c2ab57ebd9d2a03d1362b70
ufaoleg <oarezvov@gmail.com>
2017-09-10 16:08:01 +0500

    Update template.py
    replace template commit

--- template.py
+++ template.py
@@ -1,106 +1,83 @@
+# Copyright(c) 2017, Konrad K Sobon
+# @arch_laboratory, http://archi-lab.net
+
 import clr
 
-clr.AddReference('RevitAPI')
-import Autodesk
-from Autodesk.Revit.DB import *
-
-import sys
-
-# sys.path.append(r"C:\Program Files\Dynamo 0.8")
-pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
-sys.path.append(pyt_path)
-
-import os.path
-
-import math
-
-from operator import itemgetter, attrgetter
-import string
-import re
-
 clr.AddReference('ProtoGeometry')
-from Autodesk.DesignScript.Geometry import *
+import Autodesk.DesignScript.Geometry
 
-import System
-from System import Array
-from System.Collections.Generic import *
-import Autodesk.DesignScript as ds
-
-clr.AddReferenceByName(
-    'Microsoft.Office.Interop.Excel, Version=11.0.0.0, Culture=neutral, PublicKeyToken=71e9bce111e9429c')
-from Microsoft.Office.Interop import Excel
-
-System.Threading.Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo("en-US")
-from System.Runtime.InteropServices import Marshal
-
+# Import Element wrapper extension methods
 clr.AddReference("RevitNodes")
 import Revit
 
 clr.ImportExtensions(Revit.Elements)
-from Revit.Elements import *
 
+# Import geometry conversion extension methods
 clr.ImportExtensions(Revit.GeometryConversion)
-clr.ImportExtensions(Revit.GeometryReferences)
 
+# Import DocumentManager and TransactionManager
 clr.AddReference("RevitServices")
 import RevitServices
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
 
-clr.AddReference("System.Core")
-clr.ImportExtensions(System.Linq)
-
 doc = DocumentManager.Instance.CurrentDBDocument
-uidoc = DocumentManager.Instance.CurrentUIApplication.ActiveUIDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 
-view = doc.ActiveView
+# Import RevitAPI
+clr.AddReference("RevitAPI")
+from Autodesk.Revit.DB import *
+
+import System
+from System import Array
+from System.Collections.Generic import *
+
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
 
 
-def U(elem):  # короткая функция, чтобы каждый раз не писать UnwrapElement(), вместо этого достаточно U()
-    return UnwrapElement(elem)
+def process_list(_func, _list):
+    return map(lambda x: process_list(_func, x) if type(x) == list else _func(x), _list)
 
 
-# пример для получения типа стены
-walltypes = []
-if isinstance(IN[0], list):  # если на входе лист
-    for i in IN[0]:
-        walltypes.append(U(i).WallType)
-else:  # в противном случае (это означает, что на входе 1 элемент)
-    walltypes = U(IN[0]).WallType
-
-OUT = walltypes
-
-# array = ModelCurveArray() # создание массива Array
-# array.Append(UnwrapElement(i)) # положить элементы в массив
-
-# Ids=List[ElementId]() #Icollection в данном случае для ElementId
-# Ids.Add(UnwrapElement(i).Id) # добавить элементы в Icollection
-
-# получение всех элементов категории OST_Wire кроме их типов
-"""
-wires = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Wire).WhereElementIsNotElementType().ToElements()
-dstype=[]
-for i in wires:
-    dstype.append(i.ToDSType(True))
-"""
-
-# OUT = IN[0].split(‘/’) # разделить данные типа String по символу '/'
-
-# ModelCurve to Line Dynamo
-# UnwrapElement(IN[0]).GeometryCurve.ToProtoType()
-
-# Line Dynamo to ModelLine
-# ModelCurve.ByCurve(IN[0])
-
-# получение параметра по его BuiltIn значению
-# OUT = UnwrapElement(IN[0]).get_Parameter(BuiltInParameter.ELEM_FAMILY_PARAM).AsValueString()
-# в зависимости от параметра окончание может быть также AsString() AsDouble()
+def process_list_arg(_func, _list, _arg):
+    return map(lambda x: process_list_arg(_func, x, _arg) if type(x) == list else _func(x, _arg), _list)
 
 
+def process_parallel_lists(_func, *lists):
+    return map(lambda *xs: process_parallel_lists(_func, *xs) if all(type(x) is list for x in xs) else _func(*xs),
+               *lists)
+
+
+def to_list(x):
+    if hasattr(x, '__iter__'):
+        return x
+    else:
+        return [x]
+
+
+# Start Transaction
 TransactionManager.Instance.EnsureInTransaction(doc)
+# Regenerate
+TransactionManager.Instance.ForceCloseTransaction()
 
+try:
+    errorReport = None
+
+except:
+    # if error occurs anywhere in the process catch it
+    import traceback
+
+    errorReport = traceback.format_exc()
+
+# End Transaction
 TransactionManager.Instance.TransactionTaskDone()
 
-# new.ToDSType(False)
+# Assign your output to the OUT variable
+if None == errorReport:
+    OUT = 0
+else:
+    OUT = errorReport
