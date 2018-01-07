fda219333b4c1d163881af7b2f10427e1efee60a
ufaoleg <ufaoleg@bk.ru>
2018-01-07 14:30:28 +0500

    fixed issues with wrong actions

--- 01.ЛИСТЫ - ФОРМАТЫ.py
+++ 01.ЛИСТЫ - ФОРМАТЫ.py
@@ -1,90 +1,99 @@
+# coding=utf-8
+# Copyright(c) 2018, Oleg Rezvov
+# @PRSPKT Architects, http://prspkt.ru
+__author__ = "ООО АБ Проспект"
+
+# region import
 import clr
+# Import ProtoGeometry library
 clr.AddReference('ProtoGeometry')
 from Autodesk.DesignScript.Geometry import *
-#Введенные в этом узле данные сохраняется в виде списка в переменных IN.
+# Import Element wrapper extension methods
+clr.AddReference('RevitNodes')
+import Revit
+# Import DocumentManager and TransactionManager
 clr.AddReference('RevitServices')
 from RevitServices.Persistence import DocumentManager
 from RevitServices.Transactions import TransactionManager
+
+# Import RevitAPI
 clr.AddReference('RevitAPI')
 import Autodesk
 from Autodesk.Revit.DB import *
+
 from System.Collections.Generic import *
-clr.AddReference('RevitNodes')
-import Revit
+
 clr.ImportExtensions(Revit.Elements)
 import System
-doc = DocumentManager.Instance.CurrentDBDocument
+from System import Array
+from System.Collections.Generic import *
 
+import sys
+
+pyt_path = r'C:\Program Files (x86)\IronPython 2.7\Lib'
+sys.path.append(pyt_path)
+
+doc = DocumentManager.Instance.CurrentDBDocument
 uiapp = DocumentManager.Instance.CurrentUIApplication
 app = uiapp.Application
 
-sheets = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).WhereElementIsNotElementType().ToElements()
+input_razdel = IN[1]
+list_ok, list_error = [], []
+
+sheets = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_Sheets).ToElements()
 title_blocks = FilteredElementCollector(doc).OfCategory(BuiltInCategory.OST_TitleBlocks).WhereElementIsNotElementType().ToElements()
 
-list_ok, list_error = [], []
-text = IN[1]
-
-def is_not_edit(e):
+def is_not_edit(doc, e):
 	flag = False
-	name = e.LookupParameter("Редактирует").AsString()
-	if name == "" or name == app.Username:
-		flag = True
+	if doc.IsWorkshared:
+		name = e.LookupParameter("Редактирует").AsString()
+		if name == "" or name == app.Username:
+			flag = True
+	else: flag = True
 	return flag
-# j=0
 
-"""
-TODO: Реализовать выборку листов и блоков несмотря на отсеивание
-private IList<Element> m_alltitleblocks = new List<Element>();
-private IList<Element> ElementsOnSheet = new List<Element>();
-private FamilySymbol MyTitleBlock;
+def try_get_parameter(e, name, start = 0):
+	"""
+	 Проверить, существует ли такой параметр, и если да, то получить
+	 значение в текстовом виде. Иначе - None
+	"""
+	param = e.LookupParameter(name)
+	if param:
+		return e.LookupParameter(name).AsString()[start:]
+	return None
 
-foreach (Element e in new FilteredElementCollector(doc).OwnedByView(vs.Id))
-{
-ElementsOnSheet.Add(e);
-}
+def get_titleblock_on_sheet(doc, sheet):
+	"""
+	Выборка всех элементов, принадлежащих FamilyInstance на каждом листе
+	"""
+	col = FilteredElementCollector(doc)
+	out = col.OfClass(FamilyInstance).OwnedByView(sheet.Id).ToElements()[0]
+	return out.Name
 
-        public void GetAllTitleBlocks(Document doc)
-        {
-            //get all titleblocks
-            FilteredElementCollector collector = new FilteredElementCollector(doc);
-            collector.OfClass(typeof(FamilySymbol));
-            collector.OfCategory(BuiltInCategory.OST_TitleBlocks);
-
-            m_alltitleblocks = collector.ToElements();
-        }
-
-        private void GetTitleSheet(IList<Element> ElementsOnSheet)
-        {
-            foreach (Element el in ElementsOnSheet)
-            {
-                foreach (FamilySymbol Fs in m_alltitleblocks)
-                {
-                    if (el.GetTypeId().IntegerValue == Fs.Id.IntegerValue)
-                    {
-                        MyTitleBlock = Fs;
-                    }
-                }
-            }
-        }
-
-"""
-
-TransactionManager.Instance.EnsureInTransaction(doc)
-# for index, title_block in enumerate(title_blocks):
-for index, sheet in enumerate(sheets):
-	if is_not_edit(sheet):
-		razdel = sheet.LookupParameter("Раздел проекта").AsString()
-		formt = sheet.LookupParameter("Формат листа").AsString()
-		number = sheet.LookupParameter("SH_Номер листа").AsString()
-		new_number = sheet.LookupParameter("Номер листа").AsString()[1:]
-		if razdel == text:
-			sheet.LookupParameter("Формат листа").Set(title_blocks[index].ToDSType(True).ToString())
-			sheet.LookupParameter("SH_Номер листа").Set(new_number)
-			list_ok.append(sheet)
-			# j=j+1
+try:
+	errorReport = None
+	# Start Transaction
+	TransactionManager.Instance.EnsureInTransaction(doc)
+	for index, sheet in enumerate(sheets):
+		if is_not_edit(doc, sheet):
+			razdel = try_get_parameter(sheet, "Раздел проекта")
+			number = try_get_parameter(sheet, "SH_Номер листа")
+			new_number = try_get_parameter(sheet, "Номер листа", 1)
+			titlebl = get_titleblock_on_sheet(doc, sheet)
+			if razdel is not None and razdel == input_razdel:
+				sheet.LookupParameter("Формат листа").Set(titlebl)
+				list_ok.append(sheet)
+		else:
+			list_error.append(sheet)
 	else:
 		list_error.append(sheet)
-
-TransactionManager.Instance.TransactionTaskDone()
-
-OUT = list_ok, list_error
+	TransactionManager.Instance.TransactionTaskDone()
+except:  # if error accurs anywhere in the process catch it
+	import traceback
+	errorReport = traceback.format_exc()
+	# End Transaction
+	TransactionManager.Instance.TransactionTaskDone()
+if errorReport == None:
+    OUT = list_ok, list_error
+else:
+	OUT = errorReport
